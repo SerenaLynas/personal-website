0e551b0b0ba8d9ca958a63db070e631e4b0a5d6c
Serena <23511780+SerenaLynas@users.noreply.github.com>
2025-11-12 17:49:21 -0500

    Mild revisions and reordering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
         <div className='flex flex-col gap-2 pt-5'>
           <ExternalLink icon='github-mark.svg' left='GitHub' right='@SerenaLynas' href='https://github.com/SerenaLynas/'/>
           <ExternalLink icon='quam-serena.svg' left='Blog' right='quamserena.com' href='https://quamserena.com'/>
+          <ExternalLink icon='github-mark.svg' left='Sirin flight computer' right='GitHub repo' href='https://github.com/Nautki/sirin'/>
         </div>
       </div>
       <div className='p-5 gap-5 flex flex-col flex-2 bg-white h-dvh overflow-auto'>
@@ -21,25 +22,28 @@ export default function Home() {
             science and physics and applied experience in hardware and software for embedded systems — see some of my
             work below!
           </TypeCard>
-          <TypeCard title='cat education.txt'>
-            I'm a double major, expecting to finish both in May 2026:
-            <ul className='list-disc list-inside pl-4'>
-              <li><GraduationCapIcon className='inline'/> Computer Science, BS</li>
-              <li><GraduationCapIcon className='inline'/> Mathematics and Physics, BS</li>
+          <TypeCard title='cd ../case-rocket-team'>
+            <img className='h-[150px] sm:h-[300px] bg-cover bg-center rounded-lg shadow-lg float-right' src='./sirin.png'/>
+            As the avionics lead of Case Rocket Team, I designed, routed, and programmed a flight computer named Sirin and I
+            had it produced through JLCPCB. The board features an STM32H7 processor and various instruments, including
+            a LoRa radio, GPS, and barometer. The rest of the avionics subteam and I wrote the code in both Rust and C
+            (using FFI).
+            <ul className='list-disc list-inside pl-4 my-2'>
+              <li><strong><BookTextIcon className='inline'/> Languages & Libraries: </strong> Rust, C, Embassy-rs, CMSIS-DSP</li>
+              <li><strong><CpuIcon className='inline'/> Hardware & Protocols: </strong> STM32H7, LoRa, USB, SPI</li>
+              <li><strong><CircleUserIcon className='inline'/> My role: </strong> software (both robot control and React frontend)</li>
             </ul>
-          </TypeCard>
-          <TypeCard title='neofetch'>
-            Here's what I like to use:
-            <ul className='list-disc list-inside pl-4'>
-              <li><strong><MonitorCogIcon className='inline'/> OS:</strong> Linux Mint and Ubuntu</li>
-              <li><strong><FileCode2Icon className='inline'/> Languages:</strong> Rust, C/C++, JavaScript/TypeScript, Python, Java, LaTeX</li>
-              <li><strong><BookTextIcon className='inline'/> Libraries:</strong> React, SolidJS, Embassy-rs</li>
-              <li><strong><HammerIcon className='inline'/> Tools:</strong> Git, KiCad, VSCode</li>
-            </ul>
+            After flying the
+            computer on our rocket at the International Rocket Engineering Competition last summer, we are currently
+            working on a revised version for next year's competition, with new features, including sensor fusion through
+            a Kalman filter.
+            <div className='max-w-xs py-3'>
+              <ExternalLink icon='github-mark.svg' left='Sirin' right='GitHub repo' href='https://github.com/Nautki/sirin'/>
+            </div>
           </TypeCard>
           <TypeCard title='cd los-alamos'>
-            In a short 10-week internship at Los Alamos National Laboratory, I worked in a small team with a mechanical
-            engineering student and a controls student to build an automated prototype to mix polymer-bound high explosives.
+            In a 10-week internship at Los Alamos National Laboratory, I worked in a small team with a mechanical
+            engineering student and a controls student to build a robot to mix polymer-bound high explosives.
             <ul className='list-disc list-inside pl-4 my-2'>
               <li><strong><BookTextIcon className='inline'/> Languages & Libraries: </strong> Rust, TypeScript/JavaScript, React, Linux</li>
               <li><strong><CpuIcon className='inline'/> Hardware: </strong> UR16e Collaborative Robot, Raspberry Pi</li>
@@ -50,21 +54,21 @@ export default function Home() {
             recipes via the frontend. The clients were highly impressed with the functionality and polish of the system, and
             expressed interest in further development with the Department of Energy.
           </TypeCard>
-          <TypeCard title='cd ../case-rocket-team'>
-              <img className='h-[150px] sm:h-[300px] bg-cover bg-center rounded-lg shadow-lg float-right' src='./sirin.png'/>
-              As the avionics lead of Case Rocket Team, I designed, routed, and programmed a flight computer and I
-              had it produced through JLCPCB. The board features an STM32H7 processor and various instruments, including
-              a LoRa radio, GPS, and barometer. The rest of the avionics subteam and I wrote the code in both Rust and C
-              (using FFI).
-              <ul className='list-disc list-inside pl-4 my-2'>
-                <li><strong><BookTextIcon className='inline'/> Languages & Libraries: </strong> Rust, C, Embassy-rs, CMSIS-DSP</li>
-                <li><strong><CpuIcon className='inline'/> Hardware & Protocols: </strong> STM32H7, LoRa, USB, SPI</li>
-                <li><strong><CircleUserIcon className='inline'/> My role: </strong> software (both robot control and React frontend)</li>
-              </ul>
-              After flying the
-              computer on our rocket at the International Rocket Engineering Competition last summer, we are currently
-              working on a revised version for next year's competition, with new features, including sensor fusion through
-              a Kalman filter.
+          <TypeCard title='neofetch'>
+            Here's what I like to use:
+            <ul className='list-disc list-inside pl-4'>
+              <li><strong><MonitorCogIcon className='inline'/> OS:</strong> Linux Mint and Ubuntu</li>
+              <li><strong><FileCode2Icon className='inline'/> Languages:</strong> Rust, C/C++, JavaScript/TypeScript, Python, Java, LaTeX</li>
+              <li><strong><BookTextIcon className='inline'/> Libraries:</strong> React, SolidJS, Embassy-rs</li>
+              <li><strong><HammerIcon className='inline'/> Tools:</strong> Git, KiCad, VSCode</li>
+            </ul>
+          </TypeCard>
+          <TypeCard title='cat education.txt'>
+            I'm a double major, expecting to finish both in May 2026:
+            <ul className='list-disc list-inside pl-4'>
+              <li><GraduationCapIcon className='inline'/> Computer Science, BS</li>
+              <li><GraduationCapIcon className='inline'/> Mathematics and Physics, BS</li>
+            </ul>
           </TypeCard>
           <TypeCard title='' />
         </TypeCardContainer>
